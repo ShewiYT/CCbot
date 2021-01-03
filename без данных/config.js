49eba00e14b8a603439c2a53b7ae0e2f058a5eaf
ShewiYT <71594547+ShewiYT@users.noreply.github.com>
2021-01-03 16:31:55 +0700

    Add files via upload

---
+++ без данных/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    db: 'mongodb+srv://Coin:Coin@cluster0.avci8.mongodb.net/Coin?retryWrites=true&w=majority', /* MongoDB connection link */
+    secretKey: '6915ef246915ef246915ef2437696077e2669156915ef2436f45a1e48e742bb33a794e4', /* VK Mini Apps service secret key */
+    user_id: 370578631, /* VK user id */
+    token: '37bdcf244310351ab0ac0fa7455ffe57251bb6813dca72f043219b24667222f9173157d9c3f69bfa96cdd', /* VK access token */
+    key: 'JEe,oU&IifR]n*c4li;V=iJDLKfo!5l3J5J9ku3JhTcY!P3XP1' // VK Coin merchant key
+
+};
